c8fba53d42e9dcc1755ad83937c9373bfedc6154
Michał Kiełbasa <michal.kielbasa1994@gmail.com>
2022-09-27 18:17:57 +0200

    Minor changes to the look of t he product grid on the homepage.

--- src/components/GridItem.js
+++ src/components/GridItem.js
@@ -5,15 +5,15 @@ import React from "react";
 const GridItem = ({product}) => {
 
     function realPrice() {
-        if (product.price === 0) {
-            return <p>{product.price}</p>
+        if (product.discount === 0) {
+            return <span className="price-smaller">${product.price}</span>
         } else {
             return <>
                 <span className="price-smaller">
-                    <p>${product.price - (product.price * (product.discount / 100))}</p>
-                    <span className="discount-smaller"> {product.discount}% </span>
+                   ${product.price - (product.price * (product.discount / 100))}
                 </span>
-                <p className="old-price-smaller">${product.price}</p>
+                <span className="discount-smaller"> {product.discount}% </span>
+                <span className="old-price-smaller">${product.price}</span>
             </>
         }
     }
@@ -25,11 +25,11 @@ const GridItem = ({product}) => {
                        layout="fixed"
                        height="200px"
                        width="200px"
-                       style={{borderRadius: "10px"}}/>
+                       style={{borderRadius: "10px", borderBottom: 'black 1px solid'}}/>
             </Link>
             <Link href={{pathname: `/product/${product.slug}`}} key={product._id}>
                 <div className="gridDetails">
-                    <h3> {product.productName}</h3>
+                    <h3 className="product-name-smaller"> {product.productName}</h3>
                     {realPrice()}
 
                 </div>
